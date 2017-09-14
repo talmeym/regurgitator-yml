be51719e64e339c531c801bee63a8c275bfd3d7a
talmeym <mtalmey@gmail.com>
2017-09-14 18:07:46 +0100

    Generified LoaderUtils

--- src/main/java/com/emarte/regurgitator/core/YmlLoaderUtil.java
+++ src/main/java/com/emarte/regurgitator/core/YmlLoaderUtil.java
@@ -3,17 +3,21 @@ package com.emarte.regurgitator.core;
 import static com.emarte.regurgitator.core.StringUtil.dashesToCamelCase;
 import static com.emarte.regurgitator.core.YmlPackageLookup.getPackageForType;
 
-public class YmlLoaderUtil<TYPE extends Loader> extends LoaderUtil<TYPE> {
+public class YmlLoaderUtil<TYPE extends Loader> extends LoaderUtil<Yaml, TYPE> {
+
+	@Override
 	public TYPE deriveLoader(Yaml yaml) throws RegurgitatorException {
 		return buildFromClass(deriveClass(yaml));
 	}
 
-	static String deriveClass(Yaml yaml) throws RegurgitatorException {
+	@Override
+	String deriveClass(Yaml yaml) throws RegurgitatorException {
 		String type = yaml.getType();
 		return deriveClass(getPackageForType(type), dashesToCamelCase(type));
 	}
 
-	static String deriveClass(String packageName, String className) throws RegurgitatorException {
+	@Override
+	String deriveClass(String packageName, String className) throws RegurgitatorException {
 		if (packageName == null) {
 			throw new RegurgitatorException("Package not found for class: " + className);
 		}
